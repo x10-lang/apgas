bfeae94865e8be0b1ceafb7a97ade9681e11e016
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-03-19 13:59:54 +0900

    Created a GLBProcessor test class

---
+++ apgas.tests/src/apgas/glb/GLBProcessorTest.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package apgas.glb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Patrick Finnerty
+ *
+ */
+public class GLBProcessorTest {
+
+  /**
+   * Tests the merging mechanism of the GLBProcessor on a small scale
+   */
+  @Test
+  public void testMerge() {
+    fail("Not yet implemented");
+  }
+
+}

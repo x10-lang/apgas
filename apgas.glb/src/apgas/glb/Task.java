89548c3639d8c6d8804274e2392391ba5098815c
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-03-23 16:57:19 +0900

    Added a getWeight method to the Task interface

--- apgas.glb/src/apgas/glb/Task.java
+++ apgas.glb/src/apgas/glb/Task.java
@@ -12,10 +12,35 @@ package apgas.glb;
 interface Task {
   /**
    * Allows {@link TaskProcessor}s to claim this task as their own, enabling the
-   * task to call it if need be.
+   * task to call it if need be. Actually storing the given parameter
    *
    * @param p
    *          the {@link TaskProcessor} to set.
    */
   public void setTaskProcessor(TaskProcessor p);
+
+  /**
+   * Defines a weight for this task to make it possible to balance various tasks
+   * among themselves by the task processor. The GLBProcessor processes a
+   * certain amount of work before adressing potential thieves. This method
+   * allows you to decide how much your task is worth.
+   * <p>
+   * For example if your application has 3 Tasks, A B and C which are equivalent
+   * in terms of computation computation time required, you can simply choose
+   * for your tasks to return the same weight, for instance 1. The
+   * {@link GLBProcessor} will then process `workUnit` number of tasks before
+   * answering thieves.
+   * <p>
+   * However if you expect Task B to take 10 times the execution time of tasks A
+   * and C, you would choose 1, 10 and 1 as return values for Tasks A B and C
+   * respectively.
+   * <p>
+   * If you wish to take into account the specific execution that took place in
+   * your Task instance into account in order to give a specific weight which
+   * varies between instances, it is safe to do so as the {@link GLBProcessor}
+   * will call this method <em>after</em> the tasks's work has been processed.
+   *
+   * @return a positive integer representing the worth of this task
+   */
+  public int getWeight();
 }

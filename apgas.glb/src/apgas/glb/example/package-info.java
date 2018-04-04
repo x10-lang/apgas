0f1526d9f65162827a002957f6547a9833428bdf
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-04-04 15:47:47 +0900

    Added unbalanced tree search computation example using the glb api

---
+++ apgas.glb/src/apgas/glb/example/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+/**
+ * This package contains a few examples of computation performed using the glb
+ * package.
+ * 
+ * @author Patrick Finnerty
+ *
+ */
+package apgas.glb.example;

a84ed49d4a7ecb70ba45114e799c6ce8eb65d138
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-04-05 16:03:40 +0900

    Created test on the folding mechanism
    method sum provides a generic way to check for some amount to be computed
    it is used in two tests conditions : with 30 and 500 as results
    in the first case, the amount of work is too small and no steals are performed
    in the second case (500), the aount of work is sufficcient for several steals to be performed

--- apgas.tests/src/apgas/glb/GLBProcessorTest.java
+++ apgas.tests/src/apgas/glb/GLBProcessorTest.java
@@ -3,13 +3,14 @@
  */
 package apgas.glb;
 
+import static org.junit.Assert.*;
+
 import java.io.Serializable;
+import java.util.Collection;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import apgas.glb.util.TaskQueue;
-import apgas.glb.util.Task;
-import apgas.glb.util.TaskBag;
 
 /**
  * Test class for {@link apgas.glb.GLBProcessor}
@@ -20,64 +21,172 @@ import apgas.glb.util.TaskBag;
 public class GLBProcessorTest {
 
   /**
-   * Test the display of one display task
+   * GLBProcessor instance used for the tests.
    */
-  @Test
-  public void display1TaskTest() {
-    display(1, 50);
-  }
+  private static GLBProcessor processor = null;
 
-  private void display(int tasknumber, int workUnit) {
-    final TaskQueue queue = new TaskQueue();
-    for (int i = 0; i < tasknumber; i++) {
-      queue.add(new DisplayTask(Integer.toString(i)));
-    }
-
-    final GLBProcessor p = GLBProcessor.GLBProcessorFactory(workUnit, 1);
-
-    p.addTaskBag(queue);
-    p.compute();
+  /**
+   * Creates a new processor instance to be used during the tests
+   */
+  @BeforeClass
+  public static void setup() {
+    processor = GLBProcessor.GLBProcessorFactory(50, 1);
   }
 
   /**
-   * Simple Task class that implements a task which consists in displaying a
-   * String given as parameter for the constructor.
+   * Tests the functioning of the folding mechanism using the Sum class
+   * 
+   * @param value
+   *          the sum value to be obtained
+   */
+  private void sum(int value) {
+    processor.addTaskBag(new SpawnSum(value));
+
+    processor.compute();
+
+    @SuppressWarnings("rawtypes")
+    final Collection<Fold> res = processor.result();
+    assertEquals(1, res.size());
+    assert (res.toArray()[0] instanceof Sum);
+
+    final Sum s = (Sum) res.toArray()[0];
+    assertEquals(value, s.sum);
+  }
+
+  /**
+   * Resets the GLBProcessor before a new computation is performed
+   */
+  @Before
+  public void before() {
+    processor.reset();
+  }
+
+  /**
+   * Test the display of one display task
+   */
+  @Test
+  public void sumTest30() {
+    sum(30);
+  }
+
+  /**
+   * Tests the fold of 500 Sum of value 1 As 500 is above the work amount to be
+   * processed by the GLBProcessor, steal occurs.
+   */
+  @Test
+  public void sumTest500() {
+    sum(500);
+  }
+
+  /**
+   * Simple Task class that performs the sum on integers.
    *
    * @author Patrick Finnerty
    *
    */
-  private class DisplayTask implements Task, Serializable {
+  private class Sum implements Fold<Sum>, Serializable {
     /** Serial Version UID */
     private static final long serialVersionUID = -3766700434988512611L;
     /** Message to be displayed by this task */
-    String message;
+    int sum;
+
+    @Override
+    public void fold(Sum s) {
+      sum += s.sum;
+    }
 
     /**
      * Constructor
      *
-     * @param m
-     *          the String to display
+     * @param s
+     *          value to be added
      */
-    public DisplayTask(String m) {
-      message = m;
+    public Sum(int s) {
+      sum = s;
     }
+  }
 
-    /**
-     * Displays the string on the standard output
+  /**
+   * {@link Bag} used to spawn a pre-determined number of Sum {@link Fold}s
+   *
+   * @author Patrick Finnerty
+   *
+   */
+  private class SpawnSum implements Bag<SpawnSum>, Serializable {
+
+    /** Serial Version UID */
+    private static final long serialVersionUID = 6180125722875830207L;
+
+    /** Processor in charge of this Bag */
+    Processor processor;
+
+    /** Number of Sum yet to spawn */
+    int toSpawn;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#process(int)
      */
     @Override
-    public void process() {
-      System.out.println(message);
+    public void process(int workAmount) {
+      while (!isEmpty() && workAmount > 0) {
+        processor.fold(new Sum(1));
+        workAmount--;
+        toSpawn--;
+      }
     }
 
     /*
      * (non-Javadoc)
-     * 
-     * @see apgas.glb.util.Task#setProcessor(apgas.glb.util.TaskBag)
+     *
+     * @see apgas.glb.Bag#split()
      */
     @Override
-    public void setProcessor(TaskBag p) {
-      // Not used
+    public SpawnSum split() {
+      final int half = toSpawn / 2;
+      toSpawn -= half;
+      return new SpawnSum(half);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#merge(apgas.glb.Bag)
+     */
+    @Override
+    public void merge(SpawnSum b) {
+      toSpawn += b.toSpawn;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#isEmpty()
+     */
+    @Override
+    public boolean isEmpty() {
+      return toSpawn == 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#setProcessor(apgas.glb.Processor)
+     */
+    @Override
+    public void setProcessor(Processor p) {
+      processor = p;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param qtt
+     *          the number of Sum tasks to be spawned by this bag
+     */
+    public SpawnSum(int qtt) {
+      toSpawn = qtt;
     }
 
   }

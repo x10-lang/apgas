81a51b3d1dc0f6424165d68270b82e8729ca8f86
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-04-09 17:52:29 +0900

    Added test to check the proper execution of the comparatively complex UTS computation

---
+++ apgas.tests/src/apgas/glb/example/UTSTest.java
@@ -0,0 +1,75 @@
+/**
+ *
+ */
+package apgas.glb.example;
+
+import static org.junit.Assert.*;
+
+import java.security.MessageDigest;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import apgas.glb.GLBProcessor;
+
+/**
+ * @author Patrick Finnerty
+ *
+ */
+public class UTSTest {
+
+  /** Depth of the tree to be explored */
+  static final int TREE_DEPTH = 13;
+
+  /** Seed used for the tree */
+  static final int SEED = 19;
+
+  /**
+   * Expected result for the number of nodes of the tree of depth 13 and seed 19
+   */
+  static final int EXPECTED_NODES = 264459392;
+
+  /** GLBProcessor used to perform the computation */
+  static GLBProcessor processor;
+
+  /** Message dialect used by UTS computation to generate the tree */
+  static MessageDigest MD;
+
+  /** Setup method */
+  @BeforeClass
+  public static void setup() {
+    processor = GLBProcessor.GLBProcessorFactory(500, 1);
+    MD = UTSBag.encoder();
+
+    final UTSBag taskBag = new UTSBag(64);
+    processor.addTaskBag(taskBag);
+    taskBag.seed(MD, SEED, TREE_DEPTH - 2);
+
+    processor.compute();
+
+  }
+
+  /** Cleaning performed between tests */
+  @Before
+  public void beforeTest() {
+    processor.reset();
+  }
+
+  /**
+   * Tests the termination in reasonable time of the unbalanced tree search
+   * computation
+   */
+  @Test(timeout = 30000)
+  public void UTS() {
+    final UTSBag bag = new UTSBag(64);
+    processor.addTaskBag(bag);
+    bag.seed(MD, SEED, TREE_DEPTH);
+
+    processor.compute();
+
+    final long count = ((Sum) processor.result().toArray()[0]).sum;
+    assertEquals(EXPECTED_NODES, count);
+  }
+
+}
---
+++ apgas.tests/src/apgas/glb/example/package-info.java
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+/**
+ * Package to check the example of programs that use the apgas.glb library work.
+ * 
+ * @author Patrick Finnerty
+ *
+ */
+package apgas.glb.example;

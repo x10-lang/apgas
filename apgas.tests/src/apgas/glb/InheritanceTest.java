bd58091c8843b1a75371ca7b21cd353cc93d6b75
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-04-10 15:31:05 +0900

    Added a test on the inheritance management of TaskBag

---
+++ apgas.tests/src/apgas/glb/InheritanceTest.java
@@ -0,0 +1,195 @@
+/**
+ *
+ */
+package apgas.glb;
+
+import static org.junit.Assert.*;
+
+import java.io.Serializable;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import apgas.glb.example.Sum;
+
+/**
+ * @author Patrick Finnerty
+ *
+ */
+public class InheritanceTest {
+
+  /** Computation ressource used for the test */
+  static GLBProcessor processor;
+
+  /**
+   * Prepares a GLBProcessor for computation
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() {
+    processor = GLBProcessor.GLBProcessorFactory(50, 1);
+  }
+
+  /**
+   * Resets the computer back to a clean state
+   */
+  @Before
+  public void setUp() {
+    processor.reset();
+  }
+
+  /**
+   * Tests a simple inheritance situation First will spawn an AMOUNT number of
+   * Second instances which in turn are going to spawn the same amount of Sum(1)
+   * instances. Hence the final expected result of Sum is AMOUNT.
+   */
+  @Test(timeout = 5000)
+  public void inheritanceTest1() {
+    final int RESULT = 400;
+    final First bag = new First(RESULT);
+    processor.addTaskBag(bag);
+    processor.compute();
+    final Sum s = (Sum) processor.result().iterator().next();
+    assertEquals(RESULT, s.sum);
+  }
+
+  /**
+   * Parent class for {@link First} and {@link Second}
+   *
+   * @author Patrick Finnerty
+   *
+   */
+  @SuppressWarnings("serial")
+  private abstract class AbstractBag implements Bag<AbstractBag>, Serializable {
+
+    /** Data shared amongst children */
+    public int data;
+
+    /** Processor responsible for processing the tasks spawned */
+    protected Processor processor;
+
+    /**
+     * Constructor
+     * <p>
+     * Supplies the initial value for the data.
+     *
+     * @param d
+     *          initial value for data.
+     */
+    protected AbstractBag(int d) {
+      data = d;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#merge(apgas.glb.Bag)
+     */
+    @Override
+    public void merge(AbstractBag b) {
+      data += b.data;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#isEmpty()
+     */
+    @Override
+    public boolean isEmpty() {
+      return data <= 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#setProcessor(apgas.glb.Processor)
+     */
+    @Override
+    public void setProcessor(Processor p) {
+      processor = p;
+
+    }
+  }
+
+  /**
+   * Spawns {@link Second} instances as specified by the number given in the
+   * constructor.
+   *
+   * @author Patrick Finnerty
+   *
+   */
+  @SuppressWarnings("serial")
+  private class First extends AbstractBag {
+
+    /**
+     * Construcotr
+     *
+     * @param d
+     *          initial data value
+     */
+    protected First(int d) {
+      super(d);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#process(int)
+     */
+    @Override
+    public void process(int workAmount) {
+      while (data > 0 && workAmount > 0) {
+        processor.addTaskBag(new Second(1));
+        data--;
+        workAmount--;
+      }
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#split()
+     */
+    @Override
+    public AbstractBag split() {
+      final int split = data / 2;
+      data -= split;
+      return new First(split);
+    }
+  }
+
+  @SuppressWarnings("serial")
+  private class Second extends AbstractBag {
+
+    public Second(int i) {
+      super(i);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#split()
+     */
+    @Override
+    public AbstractBag split() {
+      final int split = data / 2;
+      data -= split;
+      return new Second(split);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see apgas.glb.Bag#process(int)
+     */
+    @Override
+    public void process(int workAmount) {
+      while (data > 0 && workAmount > 0) {
+        processor.fold(new Sum(1));
+        data--;
+        workAmount--;
+      }
+    }
+  }
+}

8a9d670530a00cd02088a02dfd72dd2ec1590d31
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-10-16 11:48:25 +0900

    Added missing comments

--- apgas/src/apgas/util/ResilientPlaceGroup.java
+++ apgas/src/apgas/util/ResilientPlaceGroup.java
@@ -11,10 +11,36 @@ import java.util.NoSuchElementException;
 
 import apgas.Place;
 
+/**
+ * Class meant to handle failure of a place.
+ * <p>
+ * Places are put into groups. Should one of the place fail, the group will try
+ * to recover from it.
+ */
 public class ResilientPlaceGroup implements Serializable {
+
+  /**
+   * Serial Version UID (Generated)
+   */
+  private static final long serialVersionUID = -1299742557832923401L;
+
+  /** Array containing the places that are part of the group */
   protected final Place[] array;
+
+  /**
+   * Maximum index in {@link #array} of working places. All places in the array
+   * at indeces lower or equal to {@link #max} are in use.
+   */
   protected int max;
 
+  /**
+   * Constructor
+   *
+   * The number of places to be in the group is specified as parameter
+   *
+   * @param size
+   *          the number of places to include in the group
+   */
   public ResilientPlaceGroup(int size) {
     array = new Place[size];
     int id = 0;
@@ -30,6 +56,10 @@ public class ResilientPlaceGroup implements Serializable {
     System.exit(1);
   }
 
+  /**
+   * Attempts to find a spare place in the resilient place group which is not in
+   * use yet.
+   */
   public void fix() {
     final List<? extends Place> places = places();
     final Iterator<? extends Place> it = places.iterator();
@@ -54,14 +84,34 @@ public class ResilientPlaceGroup implements Serializable {
     }
   }
 
+  /**
+   * Returns the place located at the specified index in {@link #array}
+   *
+   * @param id
+   *          the index in the array
+   * @return the place contained at the specified index
+   */
   public Place get(int id) {
     return array[id];
   }
 
+  /**
+   * Returns the size of member {@link #array}.
+   *
+   * @return size of the array
+   */
   public int size() {
     return array.length;
   }
 
+  /**
+   * Checks if the specified place is contained in the
+   * {@link ResilientPlaceGroup}.
+   *
+   * @param place
+   *          the place on which the inquiry is about.
+   * @return true is the place is in the group, false otherwise.
+   */
   public boolean contains(Place place) {
     for (int id = 0; id < array.length; ++id) {
       if (array[id].id == place.id) {
@@ -71,6 +121,14 @@ public class ResilientPlaceGroup implements Serializable {
     return false;
   }
 
+  /**
+   * Returns the index of the specified place in the {@link ResilientPlaceGroup}
+   *
+   * @param place
+   *          the place to look for.
+   * @return the index of the place in {@link #array}, {@code -1} if it is not
+   *         contained in the array.
+   */
   public int indexOf(Place place) {
     for (int id = 0; id < array.length; ++id) {
       if (array[id].id == place.id) {
@@ -80,6 +138,12 @@ public class ResilientPlaceGroup implements Serializable {
     return -1;
   }
 
+  /**
+   * Returns all the places contains in the {@link ResilientPlaceGroup} as an
+   * unmodifiable list.
+   *
+   * @return an unmodifiable list of the places contained in the group.
+   */
   public List<Place> asList() {
     return Collections.unmodifiableList(Arrays.asList(array));
   }
